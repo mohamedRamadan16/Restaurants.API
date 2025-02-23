6abe35fbc9c6b69e74835160e13d4d4cd803be49
mohamedRamadan16 <113842111+mohamedRamadan16@users.noreply.github.com>
2025-02-23 13:24:49 +0200

    Unassign Role Endpoint

--- Restaurants.API/Controllers/IdentityController.cs
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Users.Commands.AssignUserRole;
+using Restaurants.Application.Users.Commands.RemoveUserRole;
 using Restaurants.Application.Users.Commands.UpdateUserInfo;
 using Restaurants.Domain.Constants;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Restaurants.API.Controllers
 {
@@ -21,9 +23,17 @@ namespace Restaurants.API.Controllers
             return NoContent();
         }
 
-        [HttpPost("userRole")]
+        [HttpPost("AddUserRole")]
         [Authorize(Roles = RolesConstant.Admin)]
         public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
+        {
+            await mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("RemoveUserRole")]
+        [Authorize(Roles = RolesConstant.Admin)]
+        public async Task<IActionResult> RemoveUserRole(RemoveUserRoleCommand command)
         {
             await mediator.Send(command);
             return NoContent();
---
+++ Restaurants.Application/Users/Commands/RemoveUserRole/RemoveUserRoleCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Exceptions;
+
+namespace Restaurants.Application.Users.Commands.RemoveUserRole;
+
+public class RemoveUserRoleCommandHandler(ILogger<RemoveUserRoleCommandHandler> logger,
+    UserManager<User> userManager,
+    RoleManager<IdentityRole> roleManager) : IRequestHandler<RemoveUserRoleCommand>
+{
+    public async Task Handle(RemoveUserRoleCommand request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Removing Role {role} for the user with email {email}", request.RoleName, request.Email);
+
+        var user = await userManager.FindByEmailAsync(request.Email) ?? throw new NotFoundException(nameof(User), request.Email);
+        var role = await roleManager.FindByNameAsync(request.RoleName) ?? throw new NotFoundException(nameof(IdentityRole), request.RoleName);
+
+        await userManager.RemoveFromRoleAsync(user, role.Name!);
+    }
+}
